8632fc5b10751af40b89059df9abe6b34d2f23b7
leapmean <hengleapmean5@gmail.com>
2026-08-28 15:09:23 +0700

    feat: adding varieble to motocycle.

---
+++ Motorcycle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Motocycle{
+    private:
+    int id; 
+    string brand; //ex: honda or suzuki
+    string model; // ex: scoopyi or dream018
+    double price; //rental price.
+    string status; // status: available or un available.
+};
+
+int main(){
+
+}

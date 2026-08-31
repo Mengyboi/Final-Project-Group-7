16bbb4480f9012f4206c0d4bdab9e2e65fba0e2e
leapmean <hengleapmean5@gmail.com>
2026-08-31 19:47:44 +0700

    feat: adding defualtconstructor, parameterized contructor and virtual contructor.

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -2,13 +2,28 @@
 using namespace std;
 
 class Motocycle{
-    private:
-    int id; 
+private:
+    string id; 
     string brand; //ex: honda or suzuki
     string model; // ex: scoopyi or dream018
-    double price; //rental price.
-    string status; // status: available or un available.
+    double pricePerDay; //rental price.
+    bool available;// status: available or un available.
+public:
+    //defualt constructor
+    Motocycle(){
+        id = "Unknown";
+        brand = "Unknown";
+        model = "Unknown";
+        pricePerDay = 0;
+        available = true;
+    }
+    //parameterized constructor
+    Motocycle(string id, string brand, string model, double price) :
+        id(id), brand(brand), model(model), pricePerDay(price), available(true) {}
+    //virtual destructor
+    virtual ~Motocycle(){}
 };
+    
 
 int main(){
 

d87a30b122f940e5fdfcf2f34ad32e9b1eb76812
leapmean <hengleapmean5@gmail.com>
2026-08-31 20:30:47 +0700

    feat: adding virtual function for polymorphism and create a class call SportBike.

--- Motorcycle.cpp
+++ Motorcycle.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Motocycle{
+class Motorcycle{
 private:
     string id; 
     string brand; //ex: honda or suzuki
@@ -10,7 +10,7 @@ private:
     bool available;// status: available or un available.
 public:
     //defualt constructor
-    Motocycle(){
+    Motorcycle(){
         id = "Unknown";
         brand = "Unknown";
         model = "Unknown";
@@ -18,10 +18,10 @@ public:
         available = true;
     }
     //parameterized constructor
-    Motocycle(string id, string brand, string model, double price) :
+    Motorcycle(string id, string brand, string model, double price) :
         id(id), brand(brand), model(model), pricePerDay(price), available(true) {}
     //virtual destructor
-    virtual ~Motocycle(){}
+    virtual ~Motorcycle(){}
 
     //getter
     string getId(){
@@ -48,6 +48,64 @@ public:
     }
     void setPricePerDay(double pricePerday){
         this->pricePerDay = pricePerDay;
+    }
+    void setAvailable(bool available){
+        this->available = available;
+    }
+    //virtual function for polymorphism
+    virtual void displayInfo() const{
+        cout << "| ID: " << id 
+            << " | Brand: " << brand 
+            << " | Model: " << model 
+            << " | Prece/Day : $" << pricePerDay 
+            << " | Status: " << (available ? "Available" : "Rented") 
+            << endl;
+    }
+    virtual string getType() const {
+        return "sport bike";
+    }
+    
+};
+
+class SportBike : public Motorcycle {
+private:
+    int engineCC;
+
+public:
+
+    // Default constructor
+    SportBike() : Motorcycle() {
+        engineCC = 0;
+    }
+
+    // Parameterized constructor
+    SportBike(string id, string brand, string model,
+              double pricePerDay, int engineCC)
+        : Motorcycle(id, brand, model, pricePerDay) {
+
+        this->engineCC = engineCC;
+    }
+
+    // Destructor
+    ~SportBike() override {
+    }
+
+    int getEngineCC() const {
+        return engineCC;
+    }
+
+    void setEngineCC(int engineCC) {
+        this->engineCC = engineCC;
+    }
+
+    // Override virtual function
+    void displayInfo() const override {
+        cout << "[Sport Bike] "<< endl;
+        Motorcycle::displayInfo();
+    }
+
+    string getType() const override {
+        return "Sport Bike";
     }
 };
     

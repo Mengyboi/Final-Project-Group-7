efe40eb52762a7468a581832421e62c3e37c3bd2
leapmean <hengleapmean5@gmail.com>
2026-09-07 18:44:49 +0700

    feat: add setter getter and display function

--- Car.cpp
+++ Car.cpp
@@ -7,3 +7,12 @@ Car::Car() : Vehicle(){
 }
 Car::Car(string newId, string newBrand, string newModel, double priceRate, int chair) :
     Vehicle(newId, newBrand, newModel, priceRate), totalChair(chair){}
+int Car::getTotalChair(){
+    return totalChair;
+}
+void Car::setTotalChair(int chair){
+    this->totalChair = chair;
+}
+void Car::display(){
+    cout << " | Chair: " << totalChair << endl;
+}

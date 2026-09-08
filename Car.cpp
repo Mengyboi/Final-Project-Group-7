4b2d81ac9bcaa6975a9cfc3050e006f8165f2d1a
leapmean <hengleapmean5@gmail.com>
2026-09-08 13:49:59 +0700

    fix: fix bug.

--- Car.cpp
+++ Car.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 Car::Car() : Vehicle(){
-    totalChair = 0;
+    totalSeat = 0;
 }
-Car::Car(string newId, string newBrand, string newModel, double priceRate, int chair) :
-    Vehicle(newId, newBrand, newModel, priceRate), totalChair(chair){}
-int Car::getTotalChair(){
-    return totalChair;
+
+Car::Car(string newId, string newBrand, string newModel, double priceRate, int Seat) :
+    Vehicle(newId, newBrand, newModel, priceRate), totalSeat(Seat){}
+
+int Car::getTotalSeat(){
+    return totalSeat;
 }
-void Car::setTotalChair(int chair){
-    this->totalChair = chair;
+void Car::setTotalSeat(int Seat){
+    this->totalSeat = Seat;
 }
 void Car::display(){
-    cout << " | Chair: " << totalChair << endl;
+    cout << " | Chair: " << totalSeat << endl;
 }

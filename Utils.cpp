8c9471ca5ca19df6dec5968cfda3f50b4a74bc1e
Kaknika9 <kaknika189999@icloud.com>
2026-09-12 16:17:05 +0700

    feat: change output in line 21, add read a positive integer

--- Utils.cpp
+++ Utils.cpp
@@ -18,7 +18,19 @@ int Utils::readInt(string message){
         if ((ss >> value && ss.eof())){
             return value;   
         }
-        cout << "Invalid input. Please enter a number greater than 0" << endl;
-        
+        cout << "Invalid input. Please enter enter an integer." << endl;
+    }
+}
+// Read a positive integer
+int Utils::readPositiveInt(string message){
+    int value;
+    while(true){
+
+        value = readInt(message);
+    
+        if(value > 0){
+            return value;
+        }
+        cout << "Please enter a number greater than 0." << endl;
     }
 }

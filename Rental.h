c2844b2b077a911e0acf78736ce7cecb7a409a2f
leapmean <hengleapmean5@gmail.com>
2026-09-17 12:30:07 +0700

    feat: changes related to rental file

--- Rental.h
+++ Rental.h
@@ -1,41 +1,6 @@
 #ifndef RENTAL_H
 #define RENTAL_H
 
-<<<<<<< HEAD
-#include <string>
-
-class Rental {
-private:
-    int rentalId;
-    int customerId;
-    std::string vehicleId;
-    int days;
-    double totalCost;
-    bool isReturned;
-
-public:
-    Rental();
-    Rental(int id,
-           int custId,
-           const std::string& vehId,
-           int days,
-           double totalCost);
-
-    int getRentalId() const;
-    int getCustomerId() const;
-    const std::string& getVehicleId() const;
-    int getDays() const;
-    double getTotalCost() const;
-    bool getIsReturned() const;
-
-    void setDays(int days);
-    void setIsReturned(bool returned);
-
-    void displayRental() const;
-};
-
-#endif // RENTAL_H
-=======
 #include <iostream> 
 using namespace std;
 
@@ -81,4 +46,3 @@ class Rental{
 
 };
 #endif
->>>>>>> 2cb217444a0ceebf63c78afdb2b4233aa243bca2
